13df47e5b6696a5f68a28069a72efbb63d9778ca
Ian Vergara <ian.vergara@gmail.com>
2013-03-12 16:21:07 -0700

    Initial Commit

---
+++ Simple Discount Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Simple Discount Calculator
+//
+//  Created by Ian Vergara on 3/12/13.
+//  Copyright (c) 2013 Ian Vergara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Simple Discount Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Simple Discount Calculator
+//
+//  Created by Ian Vergara on 3/12/13.
+//  Copyright (c) 2013 Ian Vergara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
