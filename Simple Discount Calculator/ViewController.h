e81a9bf201122a172bf804aed2b48d23dfa29030
Ian Vergara <ian.vergara@gmail.com>
2013-03-13 22:55:18 -0700

    Added properties.

--- Simple Discount Calculator/ViewController.h
+++ Simple Discount Calculator/ViewController.h
@@ -15,6 +15,17 @@
 @property (weak, nonatomic) IBOutlet UITextField *discountField2;
 @property (weak, nonatomic) IBOutlet UITextField *salesTaxField;
 
+@property (weak, nonatomic) IBOutlet UIButton *quickDiscountButton10;
+@property (weak, nonatomic) IBOutlet UIButton *quickDiscountButton15;
+@property (weak, nonatomic) IBOutlet UIButton *quickDiscountButton25;
+@property (weak, nonatomic) IBOutlet UIButton *quickDiscountButton50;
+@property (weak, nonatomic) IBOutlet UIButton *calculateButton;
 
+@property (weak, nonatomic) IBOutlet UILabel *subtotalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *discountLabel1;
+@property (weak, nonatomic) IBOutlet UILabel *discountLabel2;
+@property (weak, nonatomic) IBOutlet UILabel *salesTaxLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 
+@property (weak, nonatomic) IBOutlet UILabel *hintLabel;
 @end
